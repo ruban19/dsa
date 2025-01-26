476d7bcad481b1a7f9b15132f493505915b609eb
Ruban Kumar <kumarruban713@gmail.com>
2025-01-26 16:07:38 +0530

    Create selection.java

---
+++ sorting/selection.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        
+        for (int i = 0 ; i < arr.length - 1; i++) {
+            int minimum_index = i;
+            for (int j = i+1 ; j <arr.length ; j++ ) {
+                if (arr[j] < arr[i]) {
+                    minimum_index = j ;
+                 }
+            }
+            int temp = arr[minimum_index];
+            arr[minimum_index] = arr[i];
+            arr[i] = temp;
+        }
+        
+        System.out.println(Arrays.toString(arr));
+    }
+}
